e6830d67931cc373cb09b5a9d9368a110c8b79b6
Meghanath91 <meghanath.balaji@gmail.com>
2021-02-12 16:20:56 -0500

    package neat-csv changed to 'csv-parser'

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 //import dependencies
 const neatCsv = require("neat-csv");
+
+const csv = require('csv-parser')
+
 const fs = require("fs");
 const firstChallengeSolution = require("./helper/firstSolution");
 const secondChallengeSolution = require("./helper/secondSolution");
@@ -9,7 +12,11 @@ const inputFile = "./sample_input.csv";
 //output files
 const firstChallengeResultFile = "firstChallengeResults.json";
 const secondChallengeResultFile = "secondChallengeResults.json";
+//to store converted javascript objects
+const results = []
 
+
+// console.log(results)
 /**
  * @func mainFunction
  * @return {undefined}
@@ -20,17 +27,19 @@ const mainFunction = async () => {
    * @return {undefined}
    */
   const readDataFromCSV = () => {
-    fs.readFile(inputFile, async (err, data) => {
-      try {
-        const javaScriptObject = await neatCsv(data);
-        const firstChallengeOutput = firstChallengeSolution(javaScriptObject);
-        const secondChallengeOutput = secondChallengeSolution(javaScriptObject);
-        writeDataToFile(firstChallengeOutput, firstChallengeResultFile);
-        writeDataToFile(secondChallengeOutput, secondChallengeResultFile);
-      } catch (err) {
-        console.log(err);
-      }
-    });
+    fs.createReadStream(inputFile)
+      .pipe(csv())
+      .on('data', (data) => results.push(data))
+      .on('end', async () => {
+        try {
+          const firstChallengeOutput = firstChallengeSolution(results);
+          const secondChallengeOutput = secondChallengeSolution(results);
+          writeDataToFile(firstChallengeOutput, firstChallengeResultFile);
+          writeDataToFile(secondChallengeOutput, secondChallengeResultFile);
+        } catch (err) {
+          console.log(err);
+        }
+      })
   };
   /**
    * @func writeDataToFile this will write results to the file
